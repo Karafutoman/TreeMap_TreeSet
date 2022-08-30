bec120b5a81b9fa999ce174e46c18f1f94018483
Andrey <109068333+Karafutoman@users.noreply.github.com>
2022-08-30 14:46:50 +1100

    Update Main.java
    Removed the comparator class and in the main method replace the object of this deleted class with a lambda with the same logic.

--- src/Main.java
+++ src/Main.java
@@ -12,19 +12,17 @@ public class Main {
         people.add(new Person("Tolya", "Zabarchuk", 41));
         people.add(new Person("Svetlana", "Li Bo Hu", 13));
 
-        people.sort(new PersonComparator(5));
-//        System.out.println(people);
-
+        people.sort((p1, p2) -> {
+            if ((p2.getSurname().split(" ").length > 2) & (p1.getSurname().split(" ").length > 2)) {
+                return p2.getAge() - p1.getAge();
+            } else {
+                return p2.getSurname().split(" ").length - p1.getSurname().split(" ").length;
+            }
+        });
+        //System.out.println(people);
         for (Person person : people) {
             System.out.println(person);
 
         }
-
-
-
-
-
-
-
     }
 }
